78283e1506358ed9b040cdd9a3a86d09ca8d0267
YanaKrukovska <jana.krua@gmail.com>
2020-06-23 00:48:13 +0300

    - Added css file for input pages - Still need to fix content of product pages hiding behind header anad footer

--- src/main/java/ua/edu/ukma/distedu/storage/controller/ApplicationController.java
+++ src/main/java/ua/edu/ukma/distedu/storage/controller/ApplicationController.java
@@ -49,16 +49,16 @@ public class ApplicationController {
 
     @GetMapping("/add-group")
     public String addGroup(Model model) {
-        if(model.getAttribute("group")==null) model.addAttribute("group", new Group());
+        if (model.getAttribute("group") == null) model.addAttribute("group", new Group());
         return "group-add";
     }
 
     @PostMapping("/request-add-group")
     public String requestAddGroup(@ModelAttribute Group group, Model model) {
         String error = isValidGroup(group);
-        if (error != null){
-            model.addAttribute("error",error);
-            model.addAttribute("group",group);
+        if (error != null) {
+            model.addAttribute("error", error);
+            model.addAttribute("group", group);
             return addGroup(model);
         }
         groupService.save(group);
@@ -66,12 +66,12 @@ public class ApplicationController {
     }
 
     @GetMapping("/products-by-group")
-    public String productsByGroup(@ModelAttribute("groupId") Long groupId,Model model) {
+    public String productsByGroup(@ModelAttribute("groupId") Long groupId, Model model) {
         model.addAttribute("groups", groupService.findAll());
         model.addAttribute("groupId", groupId);
-        if (groupId==0){
+        if (groupId == 0) {
             model.addAttribute("products", productService.findAll());
-        } else{
+        } else {
             model.addAttribute("products", productService.findAllByGroup(groupService.findGroupById(groupId)));
         }
         return "products";
@@ -80,12 +80,12 @@ public class ApplicationController {
     @GetMapping("/products")
     public String products(Model model) {
         model.addAttribute("groupId", 0);
-        return productsByGroup(0L,model);
+        return productsByGroup(0L, model);
     }
 
     @GetMapping("/add-product")
     public String addProduct(Model model) {
-        if(model.getAttribute("product")==null) model.addAttribute("product", new Product());
+        if (model.getAttribute("product") == null) model.addAttribute("product", new Product());
         model.addAttribute("groups", groupService.findAll());
         model.addAttribute("groupId", 0);
         return "product-add";
@@ -94,12 +94,12 @@ public class ApplicationController {
     @PostMapping("/request-add-product")
     public String requestAddProduct(@ModelAttribute Product product, @ModelAttribute("groupId") Long groupId, Model model) {
         String error = isValidProduct(product);
-        if(groupId==0){
+        if (groupId == 0) {
             error = "Select group";
         }
-        if (error != null){
-            model.addAttribute("error",error);
-            model.addAttribute("product",product);
+        if (error != null) {
+            model.addAttribute("error", error);
+            model.addAttribute("product", product);
             return addProduct(model);
         }
         product.setGroup(groupService.findGroupById(groupId));
@@ -117,8 +117,8 @@ public class ApplicationController {
     @PostMapping("/request-edit-group")
     public String requestEditGroup(@ModelAttribute Group group, Model model) {
         String error = isValidGroup(group);
-        if (error != null){
-            model.addAttribute("error",error);
+        if (error != null) {
+            model.addAttribute("error", error);
             return editGroup(group.getId(), model);
         }
         groupService.update(group);
@@ -138,8 +138,8 @@ public class ApplicationController {
     @PostMapping("/request-edit-product")
     public String requestEditProduct(@ModelAttribute Product product, @ModelAttribute("groupId") Long groupId, Model model) {
         String error = isValidProduct(product);
-        if (error != null){
-            model.addAttribute("error",error);
+        if (error != null) {
+            model.addAttribute("error", error);
             return editProduct(product.getId(), model);
         }
         product.setGroup(groupService.findGroupById(groupId));
@@ -154,26 +154,26 @@ public class ApplicationController {
         return "redirect:/products";
     }
 
-    private static String isValidProduct(Product product){
+    private static String isValidProduct(Product product) {
         String error = null;
-        if(product.getAmount()<0){
-            error = "Amount cannot be <0";
+        if (product.getAmount() < 0) {
+            error = "Amount cannot be < 0";
         }
-        if(product.getPrice()<0){
-            error = "Price cannot be <0";
+        if (product.getPrice() < 0) {
+            error = "Price cannot be < 0";
         }
-        if(product.getName().equals("")){
+        if (product.getName().equals("")) {
             error = "Name cannot be empty";
         }
         return error;
     }
 
-    private static String isValidGroup(Group group){
+    private static String isValidGroup(Group group) {
         String error = null;
-        if(group.getName().equals("")){
+        if (group.getName().equals("")) {
             error = "Name cannot be empty";
         }
-        if(group.getDescription().equals("")){
+        if (group.getDescription().equals("")) {
             error = "Description cannot be empty";
         }
         return error;
